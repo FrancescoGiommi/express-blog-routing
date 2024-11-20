64696e9ea2c929e92ca9874e2c8a603197bd81d9
FrancescoGiommi <francescogiommi97@icloud.com>
2024-11-20 15:12:31 +0100

    Add routers folder and posts file

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//! Giorno 1
+
 /* Esercizio
 
 Creiamo il nostro blog personale e giorno dopo giorno lo potremo arricchire 
@@ -15,6 +17,31 @@ Configuriamo gli asset statici sull’applicazione in modo che si possano
 visualizzare le immagini associate ad ogni post.
 
 Testare le chiamate su Postman! */
+
+//! Giorno 2
+
+/* Esercizio
+
+Usando l'array dei post fornito con le relative immagini, creare un file di routing (routers/posts.js) 
+che conterrà le rotte necessario per l'entità post.
+
+All'interno creare le rotte per le operazioni CRUD (Index, Show, Create, Update e Delete)
+
+Tutte le risposte saranno dei testi che confermeranno l’operazione che il server deve eseguire, 
+secondo le convenzioni REST.Ad esempio:
+
+Se viene chiamata /posts col verbo GET ci aspettiamo “Lista dei post”;
+
+Se viene chiamato /posts/1 col verbo DELETE ci aspettiamo “Cancellazione del post 1”
+e via dicendo…
+
+Registrare il router dentro app.js con il prefisso posts/.
+
+Bonus
+
+Provare a restituire la lista dei post dalla rotta index, in formato json
+
+Provare a restituire un singolo post dalla rotta show, sempre in formato json */
 
 /* Import express */
 const express = require("express");
@@ -81,4 +108,3 @@ app.get("/bacheca", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port`);
 });
-1;
